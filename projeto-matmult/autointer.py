aff135df866f976a807f94b16ea725fd652acb6e
Danilo Ramos <danilocr@al.insper.edu.br>
2025-03-23 20:28:33 -0300

    Organizando arquivo

--- projeto-matmult/autointer.py
+++ projeto-matmult/autointer.py
@@ -1,18 +1,16 @@
 import math as m
 import numpy as np
 
-
-
 def f(b, k):
     return (-14 * m.cos(((-2*m.pi * k)/10) * m.sin(b/2)) - (6 * m.cos(3*((-2*m.pi*k)/5)) * m.sin(3*b)))
+
+valores_k = [-1, -2, -3, -4, -5]
 
 intervalo_t = np.arange(0,2*m.pi,0.0008)
 
 tol = 0.001
 
 inter = []
-
-valores_k = [-1, -2, -3, -4, -5]
 
 for b in intervalo_t:
     for k in valores_k:
